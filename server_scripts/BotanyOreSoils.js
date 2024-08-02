08cf66ab63d56bc3426a1b572b3961c57232df06
Mikel Kazmer <mike.kazmer61@gmail.com>
2024-08-02 17:11:51 -0400

    adding base files needed for kubejs to work

---
+++ server_scripts/BotanyOreSoils.js
@@ -0,0 +1,247 @@
+ServerEvents.recipes(event => {
+    //vanilla addition soils
+//slime soil
+event.recipes.botanypots.soil(
+    "minecraft:slime_block",
+    {block:"minecraft:slime_block"},
+    ["slimeSoil"],
+    3000,
+    1
+)
+//brick soil
+event.recipes.botanypots.soil(
+    "minecraft:bricks",
+    {block:"minecraft:bricks"},
+    ["brickSoil"],
+    3000,
+    1
+)
+//clay soil
+event.recipes.botanypots.soil(
+    "minecraft:clay",
+    {block:"minecraft:clay"},
+    ["claySoil"],
+    3000,
+    1
+)
+//nether brick soil
+event.recipes.botanypots.soil(
+    "minecraft:nether_bricks",
+    {block:"minecraft:nether_bricks"},
+    ["netherBrickSoil"],
+    3000,
+    1
+)
+//glowstone soil
+event.recipes.botanypots.soil(
+    "minecraft:glowstone",
+    {block:"minecraft:glowstone"},
+    ["glowstoneSoil"],
+    3000,
+    1
+)
+//amethyst soil
+event.recipes.botanypots.soil(
+    "minecraft:amethyst_block",
+    {block:"minecraft:amethyst_block"},
+    ["amethystSoil"],
+    3000,
+    1
+)
+event.recipes.botanypots.soil(
+    "minecraft:budding_amethyst",
+    {block:"minecraft:budding_amethyst"},
+    ["amethystSoil"],
+    3000,
+    1
+)
+//obsidian soil
+event.recipes.botanypots.soil(
+    "minecraft:crying_obsidian",
+    {block:"minecraft:crying_obsidian"},
+    ["obsidianSoil"],
+    3000,
+    1
+)
+//vanilla ore soils
+//iron soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/iron",
+    {block:"minecraft:iron_block"},
+    ["ironSoil"],
+    3000,
+    1
+)
+//gold soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/gold",
+    {block:"minecraft:gold_block"},
+    ["goldSoil"],
+    3000,
+    1
+)
+//copper soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/copper",
+    {block:"minecraft:copper_block"},
+    ["copperSoil"],
+    3000,
+    1
+)
+//diamond soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/diamond",
+    {block:"minecraft:diamond_block"},
+    ["diamondSoil"],
+    3000,
+    1
+)
+//redstone soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/redstone",
+    {block:"minecraft:redstone_block"},
+    ["redstoneSoil"],
+    3000,
+    1
+)
+//lapis soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/lapis",
+    {block:"minecraft:lapis_block"},
+    ["lapisSoil"],
+    3000,
+    1
+)
+//quartz soil
+event.recipes.botanypots.soil(
+        "#forge:storage_blocks/quartz",
+        {block:"minecraft:quartz_block"},
+        ["quartzSoil"],
+        3000,
+        1
+    )
+//modded ore soils
+//tin soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/tin",
+    {block:"common_ore_library:tin_block"},
+    ["tinSoil"],
+    3000,
+    1
+)
+//silver soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/silver",
+    {block:"common_ore_library:silver_block"},
+    ["silverSoil"],
+    3000,
+    1
+)
+//nickel soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/nickel",
+    {block:"common_ore_library:nickel_block"},
+    ["nickelSoil"],
+    3000,
+    1
+)
+//platinum soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/platinum",
+    {block:"common_ore_library:platinum_block"},
+    ["platinumSoil"],
+    3000,
+    1
+)
+//zinc soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/zinc",
+    {block:"common_ore_library:zinc_block"},
+    ["zincSoil"],
+    3000,
+    1
+)
+//refined storage soils
+//quartz enriched iron soil
+event.recipes.botanypots.soil(
+    "refinedstorage:quartz_enriched_iron_block",
+    {block:"refinedstorage:quartz_enriched_iron_block"},
+    ["quartzEnrichedIronSoil"],
+    3000,
+    1
+)
+//ender io soils
+//copper alloy soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/copper_alloy",
+    {block:"enderio:copper_alloy_block"},
+    ["copperAlloySoil"],
+    3000,
+    1
+)
+//energetic alloy soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/energetic_alloy",
+    {block:"enderio:energetic_alloy_block"},
+    ["energeticAlloySoil"],
+    3000,
+    1
+)
+//vibrant alloy soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/vibrant_alloy",
+    {block:"enderio:vibrant_alloy_block"},
+    ["vibrantAlloySoil"],
+    3000,
+    1
+)
+//redstone alloy soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/redstone_alloy",
+    {block:"enderio:redstone_alloy_block"},
+    ["redstoneAlloySoil"],
+    3000,
+    1
+)
+//conductive alloy soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/conductive_alloy",
+    {block:"enderio:conductive_alloy_block"},
+    ["conductiveAlloySoil"],
+    3000,
+    1
+)
+//pulsating alloy soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/pulsating_alloy",
+    {block:"enderio:pulsating_alloy_block"},
+    ["pulsatingAlloySoil"],
+    3000,
+    1
+)
+//dark steel soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/dark_steel",
+    {block:"enderio:dark_steel_block"},
+    ["darkSteelSoil"],
+    3000,
+    1
+)
+//end steel soil
+event.recipes.botanypots.soil(
+    "#forge:storage_blocks/end_steel",
+    {block:"enderio:end_steel_block"},
+    ["endSteelSoil"],
+    3000,
+    1
+)
+/*//mystical agriculture soils
+//prosperity soil
+event.recipes.botanypots.soil(
+    "mysticalagriculture:prosperity_block",
+    {block:"mysticalagriculture:prosperity_block"},
+    ["prosperitySoil"],
+    3000,
+    1
+)*/
+})
